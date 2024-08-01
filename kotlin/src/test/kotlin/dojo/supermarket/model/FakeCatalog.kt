9b8661233879307ec2dc7fec3d6d652448c4e8e4
David Leitner <david.leitner@squer.io>
2024-08-01 19:03:13 +0200

    Add the best test ever Greeting da real 🐐

--- kotlin/src/test/kotlin/dojo/supermarket/model/FakeCatalog.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/FakeCatalog.kt
@@ -4,6 +4,9 @@ import supermarket.model.Product
 import supermarket.model.SupermarketCatalog
 import java.util.*
 
+class ProductNotExistingInCatalogException(product: Product) :
+    RuntimeException("Product ${product.name} does not exist in the catalog")
+
 class FakeCatalog : SupermarketCatalog {
     private val products = HashMap<String, Product>()
     private val prices = HashMap<String, Double>()
@@ -14,6 +17,6 @@ class FakeCatalog : SupermarketCatalog {
     }
 
     override fun getUnitPrice(p: Product): Double {
-        return this.prices[p.name]!!
+        return this.prices[p.name] ?: throw ProductNotExistingInCatalogException(p)
     }
 }
--- kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
@@ -3,14 +3,17 @@ package dojo.supermarket.model
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import supermarket.model.*
 
 val APPLE = Product("apples", ProductUnit.Kilo)
+val BANANA = Product("bananas", ProductUnit.Kilo)
 
 class SupermarketTest {
 
     private val catalog = FakeCatalog().apply {
         addProduct(APPLE, 1.99)
+        addProduct(BANANA, 2.99)
     }
 
     @Test
@@ -45,5 +48,29 @@ class SupermarketTest {
         val receipt = Teller(catalog).checksOutArticlesFrom(cart)
 
         assertEquals(receipt.totalPrice, 3.98)
+    }
+
+
+    @Test
+    fun `When two products are used without a discount, then the sum of both unit-prices should be the total-price`() {
+        val cart = ShoppingCart().apply {
+            addItemQuantity(APPLE, 1.0)
+            addItemQuantity(BANANA, 1.0)
+        }
+
+        val receipt = Teller(catalog).checksOutArticlesFrom(cart)
+
+        assertEquals(receipt.totalPrice, 4.98)
+    }
+
+    @Test
+    fun `When and product is added to the cart, then a useful exception should be thrown`() {
+        val cart = ShoppingCart().apply {
+            addItemQuantity(Product("unknown-product", ProductUnit.Kilo), 1.0)
+        }
+
+        assertThrows<ProductNotExistingInCatalogException> {
+            Teller(catalog).checksOutArticlesFrom(cart)
+        }
     }
 }
