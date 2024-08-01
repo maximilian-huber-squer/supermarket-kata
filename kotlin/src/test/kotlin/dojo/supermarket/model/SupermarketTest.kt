c5477fb2ec30c47e0288bc571826c52398413ddd
David Leitner <david.leitner@squer.io>
2024-08-01 18:47:48 +0200

    Add test that validate that multiple units of the same product work

--- kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
@@ -1,22 +1,49 @@
 package dojo.supermarket.model
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import supermarket.model.*
 
+val APPLE = Product("apples", ProductUnit.Kilo)
+
 class SupermarketTest {
+
+    private val catalog = FakeCatalog().apply {
+        addProduct(APPLE, 1.99)
+    }
+
     @Test
     fun `When no discounts is given for a single product, then the unit-price should be the total-price`() {
-        val catalog = FakeCatalog()
-        val apples = Product("apples", ProductUnit.Kilo)
-        catalog.addProduct(apples, 1.99)
+        val cart = ShoppingCart().apply {
+            addItemQuantity(APPLE, 1.0)
+        }
 
-        val cart = ShoppingCart()
-        cart.addItemQuantity(apples, 1.0)
-        val teller = Teller(catalog)
-
-        val receipt = teller.checksOutArticlesFrom(cart)
+        val receipt = Teller(catalog).checksOutArticlesFrom(cart)
 
         assertEquals(receipt.totalPrice, 1.99)
+    }
+
+    @Test
+    fun `When two units of the same product are used without a discount, then the unit-price multiplied by two should be the total-price`() {
+        val cart = ShoppingCart().apply {
+            addItemQuantity(APPLE, 2.0)
+        }
+
+        val receipt = Teller(catalog).checksOutArticlesFrom(cart)
+
+        assertEquals(receipt.totalPrice, 3.98)
+    }
+
+    @Test
+    fun `When the same product is used twice without a discount, then the unit-price multiplied by two should be the total-price`() {
+        val cart = ShoppingCart().apply {
+            addItemQuantity(APPLE, 1.0)
+            addItemQuantity(APPLE, 1.0)
+        }
+
+        val receipt = Teller(catalog).checksOutArticlesFrom(cart)
+
+        assertEquals(receipt.totalPrice, 3.98)
     }
 }
