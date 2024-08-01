d9550065e428a3f65a68cc37977757a2b305d64c
Maximilian Huber <maximilian.huber@squer.io>
2024-08-01 18:39:13 +0200

    add test for simple product with no discount

--- kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
+++ kotlin/src/test/kotlin/dojo/supermarket/model/SupermarketTest.kt
@@ -1,27 +1,24 @@
 package dojo.supermarket.model
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import supermarket.model.*
 
 class SupermarketTest {
 
     @Test
-    fun testSomething() {
+    fun `Normal product with no discounts`() {
         val catalog = FakeCatalog()
-        val toothbrush = Product("toothbrush", ProductUnit.Each)
-        catalog.addProduct(toothbrush, 0.99)
         val apples = Product("apples", ProductUnit.Kilo)
         catalog.addProduct(apples, 1.99)
 
         val cart =
             ShoppingCart()
-        cart.addItemQuantity(apples, 2.5)
-
+        cart.addItemQuantity(apples, 1.0)
         val teller = Teller(catalog)
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0)
 
         val receipt = teller.checksOutArticlesFrom(cart)
 
-        // Todo: complete this test
+        assertEquals(receipt.totalPrice, 1.99)
     }
 }
